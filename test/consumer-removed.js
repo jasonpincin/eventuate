2cef46cb99152795368ae5d895b46cd5d7077f96
Jason Pincin <jason@pincin.com>
2015-03-11 19:52:29 -0400

    added tests for unmonitored, and consumer{Added,Removed}

---
+++ test/consumer-removed.js
@@ -0,0 +1,28 @@
+var test      = require('tape'),
+    eventuate = require('..')
+
+test('consumer removed', function (t) {
+    t.plan(4)
+
+    function consumer1 () {}
+
+    var event1 = eventuate()
+    event1(consumer1)
+    event1.consumerRemoved(function (consumer) {
+        t.equal(consumer, consumer1, 'should be called with consumer')
+    })
+    event1.consumerRemoved().then(function (consumer) {
+        t.equal(consumer, consumer1, 'should resolve as consumer')
+    })
+    event1.remove(consumer1)
+
+    var event2 = eventuate()
+    event2.consumerRemoved(function (consumer) {
+        t.equal(consumer, undefined, 'should be called with undefined consumer when consumer is a promise')
+    })
+    event2.consumerRemoved().then(function (consumer) {
+        t.equal(consumer, undefined, 'should resolve as undefined when consumer is a promise')
+    })
+    event2()
+    event2.produce()
+})
---
+++ test/unmonitored.js
@@ -0,0 +1,33 @@
+var test      = require('tape'),
+    eventuate = require('..')
+
+test('unmonitored eventuate', function (t) {
+    t.plan(7)
+
+    var event = eventuate({ monitorConsumers: false })
+    t.equal(event.hasConsumer, undefined, 'hasConsumer should be undefined')
+
+    var consumerCallCount = 0
+    function consumer1 (value) {
+        consumerCallCount++
+        t.equal(value, 'test1', 'consumer1 should be passed value')
+        t.equal(consumerCallCount, 1, 'consumer1 should be called once')
+        event.remove(consumer1)
+    }
+    function consumer2 (value) {
+        t.equal(value, 'test1', 'consumer2 should be passed value')
+        event.remove(consumer2)
+    }
+
+    t.equal(typeof event(consumer1), 'undefined', '(consumer) should return undefined')
+    event.produce('test1')
+
+    // promise
+    t.equal(typeof event().then, 'function', '() should return promise')
+    t.equal(event(), event(), '() should return same promise')
+
+    event().then(function (value) {
+        t.equal(value, 'test2', 'promise should resolve')
+    })
+    event.produce('test2')
+})
