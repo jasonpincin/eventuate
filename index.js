d5eb72a928b077a6ecf46a344a8a600b80fb2644
Jason Pincin <jason@pincin.com>
2015-03-11 18:53:39 -0400

    Added ability to create unmonitored eventuate, added consumerRemoved, consumerAdded properties to monitored type

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 var Promise = require('promise-polyfill')
 
-module.exports = function mkEventuate () {
+module.exports = function mkEventuate (options) {
+    options = options || {}
+    options.monitorConsumers = options.monitorConsumers === undefined ? false : options.monitorConsumers
 
     var nextEvent  = null,
         _nextEvent = null,
+        monitored  = options.monitorConsumers,
         consumers  = []
 
     function eventuate (consumer) {
+        if (!consumer && nextEvent) return nextEvent
+
         if (consumer)
             consumers.push(consumer)
         else
-            return nextEvent || (nextEvent = new Promise(function (resolve,reject) {
+            nextEvent = nextEvent || (nextEvent = new Promise(function (resolve,reject) {
                 _nextEvent = {resolve:resolve, reject:reject}
             }))
+
+        if (monitored)
+            eventuate.consumerAdded.produce(consumer)
+        if (!consumer)
+            return nextEvent
     }
     eventuate.remove = function (consumer) {
         consumers.splice(consumers.indexOf(consumer), 1)
+        if (monitored)
+            eventuate.consumerRemoved.produce(consumer)
     }
     eventuate.produce = function (data) {
         consumers.forEach(function eventuateConsume(consume) {
@@ -24,14 +36,21 @@ module.exports = function mkEventuate () {
         if (_nextEvent) {
             _nextEvent.resolve(data)
             nextEvent = _nextEvent = null
+            if (monitored)
+                eventuate.consumerRemoved.produce()
         }
     }
 
-    Object.defineProperty(eventuate, "hasConsumer", {
-        get: function eventuateHasConsumer () {
-            return !!(nextEvent || consumers.length)
-        }
-    })
+    if (monitored) {
+        eventuate.consumerRemoved = mkEventuate({ monitorConsumers: false })
+        eventuate.consumerAdded   = mkEventuate({ monitorConsumers: false })
+
+        Object.defineProperty(eventuate, "hasConsumer", {
+            get: function eventuateHasConsumer () {
+                return !!(nextEvent || consumers.length)
+            }
+        })
+    }
 
     return eventuate
 }
